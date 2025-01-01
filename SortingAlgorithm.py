1e567a8e5ac77f3ab0d0cb59263cae9815677ad6
vinhthuan3108 <vinhthuan9@gmail.com>
2025-01-01 08:51:39 +0700

    Update SortingAlgorithm.py

--- SortingAlgorithm.py
+++ SortingAlgorithm.py
@@ -1,24 +1,22 @@
-class Sorter:
-    def __init__(self, data, key_index, order='asc'):
+from abc import ABC, abstractmethod
+class Sort(ABC):
+    def __init__(self, data, key_index, order):
         self.data = data
         self.key_index = key_index
         self.order = order  # 'asc' for ascending, 'desc' for descending
-
+    @abstractmethod
     def sort(self):
-        raise NotImplementedError("Sort method must be implemented by subclasses")
-
-class BubbleSorter(Sorter):
+        pass
+class BubbleSort(Sort):
     def sort(self):
         n = len(self.data)
         for i in range(n):
             swapped = False
             for j in range(0, n - i - 1):
-                # For ascending order
                 if self.order == 'asc':
                     if self.data[j][self.key_index] > self.data[j + 1][self.key_index]:
                         self.data[j], self.data[j + 1] = self.data[j + 1], self.data[j]
                         swapped = True
-                # For descending order
                 elif self.order == 'desc':
                     if self.data[j][self.key_index] < self.data[j + 1][self.key_index]:
                         self.data[j], self.data[j + 1] = self.data[j + 1], self.data[j]
@@ -26,28 +24,21 @@ class BubbleSorter(Sorter):
             if not swapped:
                 break
         return self.data
-
-class MergeSorter(Sorter):
+class MergeSort(Sort):
     def sort(self):
         return self._merge_sort(self.data)
-
     def _merge_sort(self, data):
         if len(data) <= 1:
             return data
-
         mid = len(data) // 2
         left_half = data[:mid]
         right_half = data[mid:]
-
         left_sorted = self._merge_sort(left_half)
         right_sorted = self._merge_sort(right_half)
-
         return self._merge(left_sorted, right_sorted)
-
     def _merge(self, left, right):
         result = []
         i = j = 0
-
         while i < len(left) and j < len(right):
             # For ascending order
             if self.order == 'asc':
@@ -65,7 +56,6 @@ class MergeSorter(Sorter):
                 else:
                     result.append(right[j])
                     j += 1
-
         while i < len(left):
             result.append(left[i])
             i += 1
@@ -73,15 +63,4 @@ class MergeSorter(Sorter):
         while j < len(right):
             result.append(right[j])
             j += 1
-
         return result
-    
-class HybridSorter(Sorter):
-    def sort(self):
-        # Nếu số lượng phần tử lớn , sử dụng MergeSort
-        if len(self.data) >=64:
-            return MergeSorter(self.data, self.key_index, self.order).sort()
-        # Nếu số lượng phần tử nhỏ , sử dụng BubbleSort
-        else:
-            return BubbleSorter(self.data, self.key_index, self.order).sort()
-
